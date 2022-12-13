b3aed480b333004b0c0de7ef66f1669588394ed6
EvgenijRonenko <evgenij.ronenko@gmail.com>
2022-12-13 16:53:18 +0100

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import React, {useState} from 'react';
 import './App.css';
-import {NewComponent} from './NewComponent';
+import {NewComponent2} from './NewComponent2';
+import {Button} from './Button';
 
+type FilterType = 'all'| 'Dollars'| 'RUBLS'
 
 function App() {
-   const topCars = [
-            {manufacturer:'BMW', model:'m5cs'},
-            {manufacturer:'Mercedes', model:'e63s'},
-            {manufacturer:'Audi', model:'rs6'}
-        ]
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
+const[filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money;
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
+    }
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
+    }
+    const onClickFilterHandler = (nameButton: FilterType) => {
+setFilter(nameButton)
+    }
 
     return (
-       <NewComponent topCars={topCars}/>
-    );
+<>
+    <NewComponent2/>
+</>
+)
+
 }
 
 export default App;
